bbfa7c57a4614b42d01731c4e013fc894aec4d5a
Akash Hadal <akash.hadal@gmail.com>
2024-05-13 22:55:16 +0530

    stopwatch v1.4

--- stopwatch/src/stopwatch.jsx
+++ stopwatch/src/stopwatch.jsx
@@ -30,8 +30,7 @@ const Stopwatch = () => {
   const formatTime = (timeInSeconds) => {
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = timeInSeconds % 60;
-    return `${minutes}:${seconds}`;
-
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
   };
 
   return (
