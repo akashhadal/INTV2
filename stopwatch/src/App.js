69b1b1abc8b551894f179e7cb2064208cabf06d9
Akash Hadal <akash.hadal@gmail.com>
2024-05-13 22:32:49 +0530

    stopwatch v1.0

---
+++ stopwatch/src/App.js
@@ -0,0 +1,12 @@
+
+import StopWatch from "./stopwatch";
+
+function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/stopwatch.jsx
@@ -0,0 +1,55 @@
+
+import React, { useState, useRef } from 'react';
+
+const Stopwatch = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    setIsActive(true);
+    // setInterval to increment the time every second when the stopwatch is active.
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+    //clearInterval to stop the interval timer when the stopwatch is stopped or reset
+    clearInterval(intervalRef.current);
+  };
+
+  const handleReset = () => {
+    //clearInterval to stop the interval timer when the stopwatch is stopped or reset
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setIsActive(false);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes}:${seconds}`;
+
+  };
+
+  return (
+    <div className="stopwatch" style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
+      <h1>Stopwatch</h1>
+      <p>{formatTime(time)}</p>
+      <div className="controls" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        {!isActive ? (
+          <button onClick={handleStart}>Start</button>
+        ) : (
+          <button onClick={handleStop}>Stop</button>
+        )}
+        <button onClick={handleReset} >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
